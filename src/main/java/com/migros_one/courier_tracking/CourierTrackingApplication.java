14aa529e105a92e7234feb6775c55ab70b2b11f1
uralkeser <osmanuralkeser@gmail.com>
2025-01-08 00:02:30 +0300

    initial commit

---
+++ src/main/java/com/migros_one/courier_tracking/CourierTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.migros_one.courier_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migros_one/courier_tracking/CourierTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migros_one.courier_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
